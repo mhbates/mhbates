81523a359fe2baee0b6e9ae0113bceefbf828854
Matthew Hathaway-Bates <matt@mhbates.com>
2020-05-08 21:46:14 -0700

    Move other pages to their own folders. Fix relative links in all.

--- about/index.php
+++ about/index.php
@@ -4,7 +4,7 @@
     <head>
         <meta charset="utf-8">
         <title>About Me</title>
-        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="stylesheet" type="text/css" href="../style.css">
     </head>
 
     <body>
@@ -15,20 +15,20 @@
                     <li class="right"><a href="mailto:matt@mhbates.com">Contact</a></li>
                     <li class="right"><a href="https://www.linkedin.com/in/matthew-hathaway-bates-60198865" target="_blank">LinkedIn</a></li>
                     <li class="right"><a href="https://github.com/mhbates" target="_blank">GitHub</a></li>
-                    <li class="right"><a href="projects.php">Projects</a></li>
-                    <li class="right"><a href="about.php" class="current">About</a></li>
-                    <li class="right"><a href="index.php">Home</a></li>
+                    <li class="right"><a href="../projects/">Projects</a></li>
+                    <li class="right"><a href="." class="current">About</a></li>
+                    <li class="right"><a href="..">Home</a></li>
                 </ul>
             </div>
             <div class="bodyContainer">
                 <div class="homeText">
-                    <p><img class="aboutPhoto" src="boat.png" alt="I'm on a boat!"></p> 
+                    <p><img class="aboutPhoto" src="../boat.png" alt="I'm on a boat!"></p> 
                     <p>I'm a Computer Science student at Santa Monica College, working towards a certificate and AA degree. I originally majored in Philosophy and earned a BA from UCLA in 2012.</p>
                     <p>I first became interested in computers as an 8 year-old trying to figure out Windows 95. My motivators were frustration and an incessant need to solve puzzles.</p>
                 </div>
             </div>
             <footer>
-                <?php include 'footer.php';?>
+                <?php include '../footer.php';?>
             </footer>             
         </div>
     </body>
--- projects/index.php
+++ projects/index.php
@@ -4,7 +4,7 @@
     <head>
         <meta charset="utf-8">
         <title>MHBates</title>
-        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="stylesheet" type="text/css" href="../style.css">
     </head>
 
     <body>
@@ -15,15 +15,15 @@
                     <li class="right"><a href="mailto:matt@mhbates.com">Contact</a></li>
                     <li class="right"><a href="https://www.linkedin.com/in/matthew-hathaway-bates-60198865" target="_blank">LinkedIn</a></li>
                     <li class="right"><a href="https://github.com/mhbates" target="_blank">GitHub</a></li>
-                    <li class="right"><a href="projects.php" class="current">Projects</a></li>
-                    <li class="right"><a href="about.php">About</a></li>
-                    <li class="right"><a href="index.php">Home</a></li>
+                    <li class="right"><a href="." class="current">Projects</a></li>
+                    <li class="right"><a href="../about/">About</a></li>
+                    <li class="right"><a href="..">Home</a></li>
                 </ul>
             </div>
                 <div class="bodyContainer">
                     <div class="homeText">
-                        <span><a href="http://mhbates.com/savings/" target="_blank"><img class="projectPhoto" src="simple-savings2.png"></a></span>
-                        <ul><a href="http://mhbates.com/savings/" target="_blank">Simple Savings Calculator</a>
+                        <span><a href="../savings/" target="_blank"><img class="projectPhoto" src="../simple-savings2.png"></a></span>
+                        <ul><a href="../savings/" target="_blank">Simple Savings Calculator</a>
                             <li>(js, html, css)</li>
                             <li>Calculates interest and new balance</li>
                             <li>Can compound daily, monthly, or yearly</li>
@@ -31,7 +31,7 @@
                     </div>                     
                 </div>
             <footer>
-                <?php include 'footer.php';?>
+                <?php include '../footer.php';?>
             </footer>             
         </div>
     </body>
