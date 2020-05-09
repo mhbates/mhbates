b887ee6bba4952164e6d05712223bd7093d05fbe
Matthew Hathaway-Bates <matt@mhbates.com>
2020-05-08 21:13:12 -0700

    Remember that pages need to be php files...

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="utf-8">
+        <title>MHBates</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="headerContainer">
+                <ul>
+                    <li class="left">MH Bates</li>
+                    <li class="right"><a href="mailto:matt@mhbates.com">Contact</a></li>
+                    <li class="right"><a href="https://www.linkedin.com/in/matthew-hathaway-bates-60198865" target="_blank">LinkedIn</a></li>
+                    <li class="right"><a href="https://github.com/mhbates" target="_blank">GitHub</a></li>
+                    <li class="right"><a href="projects.html">Projects</a></li>
+                    <li class="right"><a href="about.html">About</a></li>
+                    <li class="right"><a href="index.html" class="current">Home</a></li>
+                </ul>
+            </div>
+            <div class="bodyContainer">
+                <p><img class="homePhoto" src="headshot-circled.png" alt="photo"></p>
+                <div class="homeText">
+                <p>My name's Matt and I tinker with computers.</p>
+                </div>
+            </div>
+            <footer>
+                <?php include 'footer.php';?>
+            </footer>             
+        </div>
+    </body>
+</html>
