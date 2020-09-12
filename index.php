b957138255a77f00fd5f6511ea0297353b0108fd
mhbates <matt@mhbates.com>
2020-09-12 07:06:30 -0700

    Unlink the About section for now

--- index.php
+++ index.php
@@ -1,12 +1,10 @@
 <!DOCTYPE html>
 <html>
-
     <head>
         <meta charset="utf-8">
         <title>MHBates</title>
         <link rel="stylesheet" type="text/css" href="style.css">
     </head>
-
     <body>
         <div class="container">
             <div class="headerContainer">
@@ -16,7 +14,7 @@
                     <li class="right"><a href="https://www.linkedin.com/in/matthew-hathaway-bates-60198865" target="_blank">LinkedIn</a></li>
                     <li class="right"><a href="https://github.com/mhbates" target="_blank">GitHub</a></li>
                     <li class="right"><a href="projects/">Projects</a></li>
-                    <li class="right"><a href="about/">About</a></li>
+                    //<li class="right"><a href="about/">About</a></li>
                     <li class="right"><a href="." class="current">Home</a></li>
                 </ul>
             </div>
@@ -28,7 +26,7 @@
             </div>
             <footer>
                 <?php include 'footer.php';?>
-            </footer>             
+            </footer>
         </div>
     </body>
 </html>
