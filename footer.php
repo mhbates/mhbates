a962317e2125f6576d8bb6fa3accb5b93c563c41
Matthew Hathaway-Bates <matt@mhbates.com>
2021-04-02 20:49:01 -0700

    It's a new year!

--- footer.php
+++ footer.php
@@ -1,3 +1,3 @@
 <?php
-echo "<p>© 2020 Matthew Hathaway-Bates | <a href=\"https://github.com/mhbates/mhbates\" target=\"_blank\">Source</a></p>";
+echo "<p>© 2021 Matthew Hathaway-Bates | <a href=\"https://github.com/mhbates/mhbates\" target=\"_blank\">Source</a></p>";
 ?>
